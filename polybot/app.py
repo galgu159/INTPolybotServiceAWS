e34ab438921cfe468303887174352edefa33c994
galg <galguet@gmail.com>
2024-06-22 14:05:36 +0300

    without cicd

--- polybot/app.py
+++ polybot/app.py
@@ -1,13 +1,50 @@
 import flask
-from flask import request
+from flask import request, jsonify
 import os
+import boto3
+import json
+
 from bot import ObjectDetectionBot
+from loguru import logger
+from botocore.exceptions import ClientError
+
 
 app = flask.Flask(__name__)
 
 
-# TODO load TELEGRAM_TOKEN value from Secret Manager
-TELEGRAM_TOKEN = ...
+# return my secret from aws
+def get_secret():
+
+    secret_name = "TELEGRAM_TOKEN"
+    region_name = "eu-west-3"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+
+    return secret
+
+
+secret_json_str = get_secret()
+if secret_json_str:
+    secret_dict = json.loads(secret_json_str)
+    TELEGRAM_TOKEN = secret_dict.get('TELEGRAM_TOKEN')
+else:
+    print("Failed to retrieve the secret")
 
 TELEGRAM_APP_URL = os.environ['TELEGRAM_APP_URL']
 
@@ -26,15 +63,47 @@ def webhook():
 
 @app.route(f'/results', methods=['POST'])
 def results():
-    prediction_id = request.args.get('predictionId')
+    dynamodb = boto3.resource('dynamodb', region_name='eu-west-3')
+    table = dynamodb.Table('galgu-PolybotService-DynamoDB')
 
-    # TODO use the prediction_id to retrieve results from DynamoDB and send to the end-user
+    logger.info("Received request at /results endpoint")
+    try:
+        prediction_id = flask.request.args.get('predictionId')
+        if not prediction_id:
+            prediction_id = flask.request.json.get('predictionId')
 
-    chat_id = ...
-    text_results = ...
+        if not prediction_id:
+            return 'predictionId is required', 400
+        response = table.get_item(Key={'prediction_id': prediction_id})
+        if 'Item' in response:
+            item = response['Item']
+            chat_id = item['chat_id']
+            labels = item['labels']
+            unique_filename = item['unique_filename']
 
-    bot.send_text(chat_id, text_results)
-    return 'Ok'
+            text_results = f"Prediction results for image {item['original_img_path']}:\n"
+            for label in labels:
+                text_results += f"- {label['class']} at ({label['cx']:.2f}, {label['cy']:.2f}) with size ({label['width']:.2f}, {label['height']:.2f})\n"
+
+            bot.send_text(chat_id, text_results)
+
+            file_name = os.path.basename(item['unique_filename'])
+            S3_PREDICTED_URL = "https://galgu-bucket.s3.eu-north-1.amazonaws.com/"
+            s3_full_url = f"{S3_PREDICTED_URL}{file_name}"
+
+            bot.send_text(chat_id, "You can download the predicted image here:")
+            bot.send_text(chat_id, s3_full_url)
+            return 'Ok'
+        else:
+            return 'No results found', 404
+    except Exception as e:
+        print(f"Error processing results: {str(e)}")
+        return 'Error', 500
+
+
+@app.route('/health_checks/', methods=['GET'])
+def health_checks():
+    return 'Ok', 200
 
 
 @app.route(f'/loadTest/', methods=['POST'])
--- polybot/bot.py
+++ polybot/bot.py
@@ -1,8 +1,18 @@
-import telebot
-from loguru import logger
+import json
 import os
 import time
+import logging
+import boto3
+import requests
+import uuid
+import telebot
+from loguru import logger
 from telebot.types import InputFile
+from img_proc import Img
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class Bot:
@@ -67,11 +77,87 @@ class Bot:
 
 class ObjectDetectionBot(Bot):
     def handle_message(self, msg):
+        """Bot Main message handler"""
         logger.info(f'Incoming message: {msg}')
+        if "text" in msg:
+            self.send_text(msg['chat']['id'], f'Your original message: {msg["text"]}')
+        else:
+            # if there is checkbox caption
+            if "caption" in msg:
+                try:
+                    img_path = self.download_user_photo(msg)
+                    if msg["caption"] == "Blur":
+                        # Send message to telegram bot
+                        self.send_text(msg['chat']['id'], "Blur filter in progress")
+                        new_img = Img(img_path)
+                        new_img.blur()
+                        new_path = new_img.save_img()
+                        self.send_photo(msg["chat"]["id"], new_path)
+                        self.send_text(msg['chat']['id'], "Blur filter applied")
+                    elif msg["caption"] == "Contour":
+                        self.send_text(msg['chat']['id'], "Contour filter in progress")
+                        new_img = Img(img_path)
+                        new_img.contour()
+                        new_path = new_img.save_img()
+                        self.send_photo(msg["chat"]["id"], new_path)
+                        self.send_text(msg['chat']['id'], "Contour filter applied")
+                    elif msg["caption"] == "Salt and pepper":  # concat, segment
+                        self.send_text(msg['chat']['id'], "salt_n_pepper filter in progress")
+                        new_img = Img(img_path)
+                        new_img.salt_n_pepper()
+                        new_path = new_img.save_img()
+                        self.send_photo(msg["chat"]["id"], new_path)
+                        self.send_text(msg['chat']['id'], "salt_n_pepper filter applied")
+                    elif msg["caption"] == "mix":
+                        self.send_text(msg['chat']['id'], "mix filter in progress")
+                        new_img = Img(img_path)
+                        new_img.salt_n_pepper()
+                        new_path = new_img.save_img()
 
-        if self.is_current_msg_photo(msg):
-            photo_path = self.download_user_photo(msg)
+                        new_img2 = Img(new_path)
+                        new_img2.blur()
+                        new_path = new_img2.save_img()
 
-            # TODO upload the photo to S3
-            # TODO send a job to the SQS queue
-            # TODO send message to the Telegram end-user (e.g. Your image is being processed. Please wait...)
+                        self.send_photo(msg["chat"]["id"], new_path)
+                        self.send_text(msg['chat']['id'], "mix filter applied")
+                    elif msg["caption"] == "predict":
+                        self.send_text(msg['chat']['id'], "Your image is being processed. Please wait...")
+                        logger.info(f'Photo downloaded to: {img_path}')
+
+                        # Split photo name
+                        photo_s3_name = img_path.split("/")
+
+                        # Get the bucket name from the environment variable
+                        images_bucket = os.environ['BUCKET_NAME']
+
+                        # Upload the image to S3
+                        s3_client = boto3.client('s3')
+                        s3_client.upload_file(img_path, images_bucket, photo_s3_name[-1])
+
+                        # Prepare the data to be sent to SQS
+                        prediction_id = str(uuid.uuid4())
+                        json_data = {
+                            'imgName': img_path,
+                            'chat_id': msg['chat']['id'],
+                            'prediction_id': prediction_id
+                        }
+
+                        try:
+                            # Send job to queue
+                            sqs = boto3.client('sqs', region_name='eu-west-3')
+                            sqs_queue_url = 'https://sqs.eu-west-3.amazonaws.com/019273956931/galgu-PolybotServiceQueue'
+                            response = sqs.send_message(
+                                QueueUrl=sqs_queue_url,
+                                MessageBody=json.dumps(json_data)
+                            )
+                            self.send_text(msg['chat']['id'], f"Job sent to queue. PredictionId: {prediction_id}")
+                        except Exception as e:
+                            logger.error(f'Error: {str(e)}')
+                            self.send_text(msg.chat.id, 'Failed to process the image. Please try again later.')
+                    else:
+                        self.send_text(msg['chat']['id'], "Error invalid caption\n Available captions are :\n 1)Blur\n2)mix\n3)Salt and pepper\n 4)predict")
+                except Exception as e:
+                    logger.info(f"Error {e}")
+                    self.send_text(msg['chat']['id'], f'failed - try again later')
+            else:
+                self.send_text(msg['chat']['id'], "please provide caption")
